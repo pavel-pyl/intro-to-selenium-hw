80803ac3cedf99ee8a44439c8a90641ae8b66445
ext-deviget-pavlo.pylypenko <pavlo.pylypenko@ringteam.com>
2022-06-18 16:21:19 +0300

    Silenium part 2

--- src/main/java/com/example/selenium/pages/MainPage.java
+++ src/main/java/com/example/selenium/pages/MainPage.java
@@ -1,22 +1,31 @@
 package com.example.selenium.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
 
 public class MainPage extends com.example.selenium.pages.BasePage {
 
-    // TODO define title and logout WebElements using @FindBy
+    @FindBy(how = How.CSS, using = "h2")
+    private WebElement titleLocator;
+    @FindBy(how = How.CSS, using = ".icon-2x")
+    private WebElement logoutLocator;
+
 
     public MainPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public String getTitle() {
-        // TODO return title text
-        return null;
+        return titleLocator.getText();
     }
 
     public MainPage clickLogout() {
-        // TODO click logout and return MainPage
+        logoutLocator.click();
         return this;
     }
 }
--- src/main/java/com/example/selenium/steps/UserSteps.java
+++ src/main/java/com/example/selenium/steps/UserSteps.java
@@ -1,8 +1,14 @@
 package com.example.selenium.steps;
 
+import com.example.selenium.pages.LoginPage;
+import com.example.selenium.pages.MainPage;
 import org.openqa.selenium.WebDriver;
 
+import static org.testng.Assert.assertEquals;
+
 public class UserSteps {
+    LoginPage loginPage;
+    MainPage mainPage;
     private WebDriver driver;
 
     public UserSteps(WebDriver driver) {
@@ -10,14 +16,14 @@ public class UserSteps {
     }
 
     public com.example.selenium.steps.UserSteps login(String username, String password) {
-        // TODO initialize LoginPage, call setUsername, setPassword, and click login methods
-
-        //  TODO assert title
+        loginPage = new LoginPage(driver);
+        mainPage = loginPage.setUsername(username).setPassword(password).clickLogin();
+        assertEquals(mainPage.getTitle(), "Secure Area", "Login failed");
         return this;
     }
 
     public UserSteps logout() {
-        // TODO call logout method
+        mainPage.clickLogout();
         return this;
     }
 }
